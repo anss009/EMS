b07a148afe1608b121b19f6545559e9751018598
Muhammad Anss <ansslucky07@gmail.com>
2026-03-14 23:59:08 +0200

    Update Login.jsx

---
+++ EMS/src/components/Auth/Login.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export const Login = () => {
+  return (
+    <div className='flex item-center justify-center h-screen'>
+        <div>
+
+        </div>
+    </div>
+  )
+}
