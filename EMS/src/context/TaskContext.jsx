64a4eef77cb9570637529d7b101ab9dc35419bdf
Muhammad Anss <ansslucky07@gmail.com>
2026-03-23 23:51:51 +0200

    Update TaskContext.jsx

--- EMS/src/context/TaskContext.jsx
+++ EMS/src/context/TaskContext.jsx
@@ -1,9 +1,9 @@
-import React from 'react'
+import React, { Children } from 'react'
 
-const TaskContext = () => {
+const TaskContext = ({Children}) => {
   return (
     <div>
-      
+      {Children}
     </div>
   )
 }
