ef332de2f422338fe368e6d5523871d538489c39
Muhammad Anss <ansslucky07@gmail.com>
2026-03-23 23:39:54 +0200

    new components

---
+++ EMS/src/components/Other/AllTask.jsx
@@ -0,0 +1,51 @@
+import React from 'react'
+
+const AllTask = () => {
+  return (
+    <div className=' bg-[#1c1c1c] mt-5 rounded p-5 h-48 overflow-auto'>
+      <div className=' bg-red-400 py-2 px-4 flex justify-between mb-2 '>
+        <h2>Anss</h2>
+        <h3>Make A UI Design</h3>
+        <h5>Status</h5>
+      </div>
+      <div className=' bg-red-400 py-2 px-4 flex justify-between mb-2 '>
+        <h2>Anss</h2>
+        <h3>Make A UI Design</h3>
+        <h5>Status</h5>
+      </div>
+      <div className=' bg-red-400 py-2 px-4 flex justify-between mb-2 '>
+        <h2>Anss</h2>
+        <h3>Make A UI Design</h3>
+        <h5>Status</h5>
+      </div>
+      <div className=' bg-red-400 py-2 px-4 flex justify-between mb-2 '>
+        <h2>Anss</h2>
+        <h3>Make A UI Design</h3>
+        <h5>Status</h5>
+      </div>
+      <div className=' bg-red-400 py-2 px-4 flex justify-between mb-2 '>
+        <h2>Anss</h2>
+        <h3>Make A UI Design</h3>
+        <h5>Status</h5>
+      </div>
+       
+      <div className=' bg-red-400 py-2 px-4 flex justify-between mb-2 '>
+        <h2>Anss</h2>
+        <h3>Make A UI Design</h3>
+        <h5>Status</h5>
+      </div>
+      <div className=' bg-red-400 py-2 px-4 flex justify-between mb-2 '>
+        <h2>Anss</h2>
+        <h3>Make A UI Design</h3>
+        <h5>Status</h5>
+      </div>
+      <div className=' bg-red-400 py-2 px-4 flex justify-between mb-2 '>
+        <h2>Anss</h2>
+        <h3>Make A UI Design</h3>
+        <h5>Status</h5>
+      </div>
+    </div>
+  )
+}
+
+export default AllTask
--- EMS/src/components/TaskList/TaskList.jsx
+++ EMS/src/components/TaskList/TaskList.jsx
@@ -2,8 +2,55 @@ import React from 'react'
 
 const TaskList = () => {
   return (
-    <div className='h-55% w-full bg-red-500 mt-10 '>
-      <div className='h-full w-[300px] bg-yellow-400 rounded-r-2xl'> </div>
+    <div id='tasklist' className='h-[55%] w-full overflow-x-auto flex items-center justify-self-start  gap-5 flex-nowrap  mt-10 '>
+      <div className=' h-full flex-shrink-0  w-[300px] bg-red-400 p-5 rounded-xl'>
+        <div className=' flex justify-between items-center '>
+          <h3 className='bg-red-600 px-3 py-1 rounded text-sm '>High</h3>
+          <h4 className='text-sm'>23 march 2026</h4>
+        </div>
+        <div>
+          <h2 className='mt-5 text-2xl font-semibold'>Make a youtube video</h2>
+          <p className='text-sm mt-2'>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea dignissimos, commodi reprehenderit molestiae totam labore.
+          </p>
+        </div>
+      </div>
+      <div className=' h-full flex-shrink-0  w-[300px] bg-cyan-400 p-5 rounded-xl'>
+        <div className=' flex justify-between items-center '>
+          <h3 className='bg-red-600 px-3 py-1 rounded text-sm '>High</h3>
+          <h4 className='text-sm'>23 march 2026</h4>
+        </div>
+        <div>
+          <h2 className='mt-5 text-2xl font-semibold'>Make a youtube video</h2>
+          <p className='text-sm mt-2'>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea dignissimos, commodi reprehenderit molestiae totam labore.
+          </p>
+        </div>
+      </div>
+      <div className=' h-full flex-shrink-0  w-[300px] bg-purple-400 p-5 rounded-xl'>
+        <div className=' flex justify-between items-center '>
+          <h3 className='bg-red-600 px-3 py-1 rounded text-sm '>High</h3>
+          <h4 className='text-sm'>23 march 2026</h4>
+        </div>
+        <div>
+          <h2 className='mt-5 text-2xl font-semibold'>Make a youtube video</h2>
+          <p className='text-sm mt-2'>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea dignissimos, commodi reprehenderit molestiae totam labore.
+          </p>
+        </div>
+      </div>
+      <div className=' h-full flex-shrink-0  w-[300px] bg-orange-400 p-5 rounded-xl'>
+        <div className=' flex justify-between items-center '>
+          <h3 className='bg-red-600 px-3 py-1 rounded text-sm '>High</h3>
+          <h4 className='text-sm'>23 march 2026</h4>
+        </div>
+        <div>
+          <h2 className='mt-5 text-2xl font-semibold'>Make a youtube video</h2>
+          <p className='text-sm mt-2'>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea dignissimos, commodi reprehenderit molestiae totam labore.
+          </p>
+        </div>
+      </div>
     </div>
   )
 }
