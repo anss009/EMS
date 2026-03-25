9ea45706bccda2304a2b20199be61c91fdc396fa
Muhammad Anss <ansslucky07@gmail.com>
2026-03-25 21:09:07 +0200

    Update LocalStorage.jsx

--- EMS/src/utilites/LocalStorage.jsx
+++ EMS/src/utilites/LocalStorage.jsx
@@ -1,11 +1,27 @@
-import React from 'react'
-
-const LocalStorage = () => {
-  return (
-    <div>
-      
-    </div>
-  )
-}
-
-export default LocalStorage
+const employee = [
+  {
+    "id": 1,
+    "email": "employee1@company.com",
+    "password": "112233"
+  },
+  {
+    "id": 2,
+    "email": "employee2@company.com",
+    "password": "112233"
+  },
+  {
+    "id": 3,
+    "email": "employee3@company.com",
+    "password": "112233"
+  },
+  {
+    "id": 4,
+    "email": "employee4@company.com",
+    "password": "112233"
+  },
+  {
+    "id": 5,
+    "email": "employee5@company.com",
+    "password": "112233"
+  }
+]
