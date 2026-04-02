8bd7a9dd8bd8ab2292b5c205fc76bc148b673dcf
Muhammad Anss <ansslucky07@gmail.com>
2026-04-02 20:29:26 +0300

    .

--- EMS/src/App.jsx
+++ EMS/src/App.jsx
@@ -17,27 +17,31 @@ const App = () => {
 },[ ])
 
     const [user, setUser] = useState(null)
+    const [loggedInUserData, setloggedInUserData] = useState(null)
     const authData = useContext(AuthContext)
     console.log(authData)
 
-    useEffect(()=>{
-        const loggedInUser = localStorage.getItem('loggedInUser')
-        if(loggedInUser){
-          setUser(loggedInUser)
-          if(loggedInUser){
-            setUser(loggedInUser.role)
-          }
-        } 
-    },[])
+    // useEffect(()=>{
+    //     const loggedInUser = localStorage.getItem('loggedInUser')
+    //     if(loggedInUser){
+    //       setUser(loggedInUser)
+    //       if(loggedInUser){
+    //         setUser(loggedInUser.role)
+    //       }
+    //     } 
+    // },[])
 
 
     const handleLogin = (email, password)=>{
       if(email =="admin@company.com"  && password  == '112233'){
         setUser('admin')
         localStorage.setItem('loggedInUser', JSON.stringify({role:'admin'}))
-      }else if (authData && authData.employees.find((e)=> e.email == email && e.password == password)){
-        setUser('Employee')
-        localStorage.setItem('loggedInUser', JSON.stringify({role:'employee'}))
+      }else if (authData){
+        const employee = authData.employees.find((e)=> e.email == email && e.password == password)
+        if(employee){   
+          setUser('employee')
+          localStorage.setItem('loggedInUser', JSON.stringify({role:'employee'}))
+        }
       }
       else{
         alert("Invalid Crendential")
--- EMS/src/components/Dashboard/EmployeeDashboard.jsx
+++ EMS/src/components/Dashboard/EmployeeDashboard.jsx
@@ -3,7 +3,8 @@ import Header from '../Other/Header'
 import TaskListNumber from '../Other/TaskListNumber'
 import TaskList from '../TaskList/TaskList'
 
-export const EmployeeDashboard = () => {
+export const EmployeeDashboard = (props) => {
+  console.log(props)
   return (
     <div className='p-15 bg-[#1c1c1c] h-screen'>
       <Header/>
